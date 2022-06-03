b87f5800d2a3a0beef92dee28c1d205a0b912bc9
sarkermajid <majidursarkar333@gmail.com>
2022-06-03 16:45:26 +0600

    restfull api generate for sendmail

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\TestMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class MailController extends Controller
+{
+    public function send_mail(Request $request){
+        if($request->has('email')){
+            $email = $request->get('email');
+            $password = rand(100001, 999999);
+
+            $details = [
+                'title' => 'Send User Password',
+                'body' => 'Your password is <b>'. $password . '</b>',
+            ];
+            Mail::to($email)->send(new TestMail($details));
+            return json_encode(['success' => true, 'response_code' => 200]);
+        }else{
+            return json_encode(['success' => false, 'response_code' => 404]);
+        }
+    }
+}
